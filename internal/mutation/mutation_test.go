0eae27202877826b3f52b64feb8514e86092e8de
Tyler Kendrick <145080887+Tyler-R-Kendrick@users.noreply.github.com>
2026-08-25 20:11:07 -0500

    test(http): mutate unmerged request limits

--- internal/mutation/mutation_test.go
+++ internal/mutation/mutation_test.go
@@ -270,6 +270,14 @@ func TestMutantsAreKilled(t *testing.T) {
 			run:  "TestStatesHTTPTaskAndTrace",
 		},
 		{
+			name: "http-allow-oversized-unmerged-request",
+			file: filepath.Join("internal", "services", "aws", "eventhttp", "eventhttp.go"),
+			old:  `if limit > 0 && int64(len(body)) > limit {`,
+			new:  `if false {`,
+			pkg:  "./internal/services/aws/eventhttp",
+			run:  "TestMergeBodyLimitWithoutConnectionParameters",
+		},
+		{
 			name: "states-expose-http-task-form-secrets",
 			file: filepath.Join("internal", "services", "aws", "eventhttp", "eventhttp.go"),
 			old:  `traceBody = call.Body`,

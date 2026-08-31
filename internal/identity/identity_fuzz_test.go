99ccafc98337a9e5648ec9ebec8434b9bb0c80b7
Tyler Kendrick <145080887+Tyler-R-Kendrick@users.noreply.github.com>
2026-08-31 03:44:11 -0500

    test(s3): fuzz malformed credentials

--- internal/identity/identity_fuzz_test.go
+++ internal/identity/identity_fuzz_test.go
@@ -3,6 +3,7 @@ package identity
 import (
 	"net/http/httptest"
 	"net/url"
+	"strings"
 	"testing"
 	"time"
 )
@@ -22,5 +23,21 @@ func FuzzParse(f *testing.F) {
 		v2 := httptest.NewRequest("GET", "/x?AWSAccessKeyId=test&Expires="+url.QueryEscape(cred), nil)
 		_, _ = PresignedExpiry(v2)
 		_ = PresignedAuthFault(v2)
+	})
+}
+
+func FuzzPresignedCredentialSyntax(f *testing.F) {
+	f.Add("test/20200101/us-east-1/s3/aws4_request")
+	f.Add("test%2F20200101%2Fus-east-1%2Fs3%2Faws4_request")
+	f.Fuzz(func(t *testing.T, credential string) {
+		target := "/x?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=" + url.QueryEscape(credential) + "&X-Amz-Signature=00&X-Amz-Date=20200101T000000Z&X-Amz-Expires=60&X-Amz-SignedHeaders=host"
+		fault := PresignedAuthFault(httptest.NewRequest("GET", target, nil))
+		if len(strings.Split(credential, "/")) == 5 {
+			if fault != nil {
+				t.Fatalf("valid credential rejected: %#v", fault)
+			}
+		} else if fault == nil || fault.Code != "AuthorizationQueryParametersError" {
+			t.Fatalf("malformed credential accepted: %#v", fault)
+		}
 	})
 }

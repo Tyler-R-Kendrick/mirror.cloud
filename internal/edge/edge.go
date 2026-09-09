cfa6eba308fb50e87c5be0cc90ca3420edf42294
Tyler Kendrick <145080887+Tyler-R-Kendrick@users.noreply.github.com>
2026-09-09 17:01:48 -0500

    fix(sns): isolate signing certificate dependency

--- internal/edge/edge.go
+++ internal/edge/edge.go
@@ -27,7 +27,7 @@ import (
 	"github.com/tyler-r-kendrick/mirror.cloud/internal/proto/aws/restxml"
 	"github.com/tyler-r-kendrick/mirror.cloud/internal/proto/gcp/gcprest"
 	"github.com/tyler-r-kendrick/mirror.cloud/internal/registry"
-	"github.com/tyler-r-kendrick/mirror.cloud/internal/services/aws/sns"
+	"github.com/tyler-r-kendrick/mirror.cloud/internal/services/aws/sns/cert"
 	"github.com/tyler-r-kendrick/mirror.cloud/internal/spi"
 )
 
@@ -80,7 +80,7 @@ func New(cfg config.Config, deps spi.Deps, reg registry.Registry, version string
 func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if strings.HasSuffix(r.URL.Path, "/_aws/sns/SimpleNotificationService.pem") {
 		w.Header().Set("Content-Type", "application/x-pem-file")
-		_, _ = w.Write(sns.SigningCertificate())
+		_, _ = w.Write(cert.Certificate())
 		return
 	}
 	var awsChunks [][]byte
---
+++ internal/services/aws/sns/cert/cert.go
@@ -0,0 +1,63 @@
+package cert
+
+import (
+	"crypto"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha1"
+	"crypto/sha256"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/base64"
+	"encoding/pem"
+	"math/big"
+	"strings"
+	"sync"
+	"time"
+)
+
+var signing struct {
+	once sync.Once
+	key  *rsa.PrivateKey
+	cert []byte
+}
+
+func initSigning() {
+	signing.key, _ = rsa.GenerateKey(rand.Reader, 2048)
+	now := time.Now().UTC()
+	template := &x509.Certificate{SerialNumber: big.NewInt(1), Subject: pkix.Name{CommonName: "Mirror SNS"}, NotBefore: now.Add(-time.Minute), NotAfter: now.AddDate(10, 0, 0), KeyUsage: x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign, IsCA: true}
+	der, _ := x509.CreateCertificate(rand.Reader, template, template, &signing.key.PublicKey, signing.key)
+	signing.cert = pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: der})
+}
+
+func Certificate() []byte {
+	signing.once.Do(initSigning)
+	return signing.cert
+}
+
+func Sign(values map[string]any, version string, stringify func(any) string) string {
+	signing.once.Do(initSigning)
+	fields := []string{"Message", "MessageId", "Subject", "Timestamp", "TopicArn", "Type"}
+	var b strings.Builder
+	for _, field := range fields {
+		if value, ok := values[field]; ok {
+			b.WriteString(field)
+			b.WriteByte('\n')
+			b.WriteString(stringify(value))
+			b.WriteByte('\n')
+		}
+	}
+	var hash crypto.Hash
+	var digest []byte
+	if version == "2" {
+		hash = crypto.SHA256
+		sum := sha256.Sum256([]byte(b.String()))
+		digest = sum[:]
+	} else {
+		hash = crypto.SHA1
+		sum := sha1.Sum([]byte(b.String()))
+		digest = sum[:]
+	}
+	signature, _ := rsa.SignPKCS1v15(rand.Reader, signing.key, hash, digest)
+	return base64.StdEncoding.EncodeToString(signature)
+}
